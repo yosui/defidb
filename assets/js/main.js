cfb1c1d41dbe6a823978956479d2f714c9445c2a
yosui <yosui262@gmail.com>
2020-05-24 13:12:25 +0900

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+Vue.component('project',{
+    props: ['title'],
+    template: `<div>{{title}}</div>`,
+    data(){
+        return{
+            
+        }
+    }
+});
+
+
+var app = new Vue({ el:'#index-list' });
+
+
+
+
+/* anime.js */
